1ea680a6c2e76438c2c346ed687398413a108a41
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# GridView - How to display master-detail tables in two grids on separate tabs of a PageControl
+
+
+<p>This example demonstrates how to use two GridView extensions to display related tables. The GridView extensions are located on the PageControl TabPages. To filter data in the second (detail) GridView, focus a row of the first (master) GridView and activate the second TabPage. </p><p><strong>Note</strong> that the detail GridView does not get filtered until you change the active TabPage. This was made for performance reasons.</p>
+
+<br/>
+
+
---
+++ VB/Controllers/HomeController.vb
@@ -0,0 +1,26 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.IO
+Imports System.Web
+Imports System.Web.Mvc
+
+Namespace CS.Controllers
+	Public Class HomeController
+		Inherits Controller
+		Public Function Index() As ActionResult
+            ViewBag.Message = "Welcome to DevExpress Extensions for ASP.NET MVC!"
+            ViewData("CategoryList") = MyModel.GetCategories()
+            ViewData("ProductList") = MyModel.GetProducts()
+
+            Return View()
+        End Function
+
+        Public Function GridCategoriesAction() As ActionResult
+            Return PartialView("GridCategories", MyModel.GetCategories())
+        End Function
+        Public Function GridProductsAction(CategoryID As System.Nullable(Of Integer)) As ActionResult
+            Return PartialView("GridProducts", MyModel.GetProductsByCategory(CategoryID))
+        End Function
+
+	End Class
+End Namespace
